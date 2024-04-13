12117c044288d25bf89c18fb34dc6daafac5c46c
Sharang Ahirekar <sharangahirekar1@gmail.com>
2024-04-13 19:17:26 +0530

    saving passwords as hash

--- todo-back/src/controllers/users.js
+++ todo-back/src/controllers/users.js
@@ -1,17 +1,36 @@
 const User = require("../schemas/user.schema");
+const crypto = require("crypto");
 
+const createHash = (password, salt) => {
+    const hash = crypto.createHmac('sha512', salt);
+    hash.update(password);
+    return hash.digest('hex');
+}
+
+const salt = "0123456789";
 
 const controls = {};
 
 controls.signup = async (req,res) => {
     // checking if user already exists
-    const email = req.body.email;
+    const userData = req.body;
+    const email = userData.email;
+    const hash = createHash(userData.password, salt);
+    delete userData.password;
+    delete userData.confirm_password;
+    userData.hash = hash;
+
+
+    console.log(userData, 'form data');
+
     const userExists = await User.find({email});
-    if(userExists){
+    console.log(userExists,'existing user data from mongo');
+    if(userExists.length > 0){
+
         return res.send("User exists!");
     }
 
-    const user = new User(req.body);
+    const user = new User(userData);
     await user.save();
     res.send(user);
 }
