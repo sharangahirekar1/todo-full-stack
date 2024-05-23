46cf8e75f445932c061c41bbdecbdf35d6696a82
Sharang Ahirekar <sharangahirekar1@gmail.com>
2024-05-23 10:10:39 +0530

    added sentry io

--- todo-back/src/index.js
+++ todo-back/src/index.js
@@ -1,3 +1,4 @@
+require("./instrument");
 const express = require("express");
 const mongoose = require("mongoose");
 const todoSlice = require("./routes/todo.route");
@@ -7,18 +8,29 @@ const cors = require("cors")
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const Sentry = require("@sentry/node");
 dotenv.config();
 
 const connStr = process.env.MONGODB_CONNECTION_STR;
 console.log(connStr, 'connection string');
 const app = express();
+
 app.use(morgan("combined"));
 app.use(cookieParser());
 app.use(express.json({limit: '50mb'})) // if not validation will fail
 app.use(cors())
+
 app.use("/todos",todoSlice);
 app.use("/users",userSlice);
 app.use("/genai",genaiSlice);
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+  });
+Sentry.setupExpressErrorHandler(app)
+app.use(function onError(err, req,res, next){
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+})
 
 app.listen(8111,async()=>{
     await mongoose.connect(connStr);
