40b903e8854cd742f8fdc9639ccf8629d7c9c5d1
Sharang Ahirekar <sharangahirekar1@gmail.com>
2024-06-10 22:34:15 +0530

    local production urls toggle script added

--- todo-front/deploy.js
+++ todo-front/deploy.js
@@ -10,7 +10,16 @@ fs.readFile(__dirname + '/src/Account/state/actions.js','utf-8',(err, data) => {
     
         fs.writeFile(__dirname + '/src/Account/state/actions.js',d.join("\r\n"), (err) => {
             if (err) throw err;
-            else console.log("Write successfull");
+            else console.log("Production urls");
+        })
+    }
+    else if (d[3][0] == "/"){
+        d[4] = "// " + d[4];
+        d[3] = d[3].slice(3)
+    
+        fs.writeFile(__dirname + '/src/Account/state/actions.js',d.join("\r\n"), (err) => {
+            if (err) throw err;
+            else console.log("Local urls");
         })
     }
 
@@ -25,7 +34,16 @@ fs.readFile(__dirname + '/src/Todo/state/actions.js','utf-8',(err, data) => {
         d[4] = d[4].slice(3)
         fs.writeFile(__dirname + '/src/Todo/state/actions.js', d.join("\r\n"), (err) => {
             if (err) throw err;
-            else console.log("Write successfully");
+            else console.log("Production urls");
+            // console.log(d.join("\r\n"))
+        })
+    }
+    else if (d[3][0] == "/"){
+        d[4] = "// " + d[4];
+        d[3] = d[3].slice(3)
+        fs.writeFile(__dirname + '/src/Todo/state/actions.js', d.join("\r\n"), (err) => {
+            if (err) throw err;
+            else console.log("Local urls");
             // console.log(d.join("\r\n"))
         })
     }
